8696aa6e2aec7c144882b96c4334c6dcb3b3c156
Tracie T <traciecodes@gmail.com>
2019-07-23 17:34:38 -0500

    Stretch step 1 complete. So DRY

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 const Movie = (props) => {
@@ -24,36 +25,20 @@ const Movie = (props) => {
   }, []);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
 
   const { title, director, metascore, stars } = movie;
-  // const newStars = Array.from(stars);
-
-  return (
+    return(
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      
+      <MovieCard title={title} director={director} metascore={metascore} stars={stars} />
       <div className="save-button">Save</div>
     </div>
   );
